9d773f6fecc96e95131fd9c28945cbe57a09a7c5
Erarshad <www.moarshad310@gmail.com>
2024-06-18 17:34:32 +0530

    added

--- app/app_insight/page.tsx
+++ app/app_insight/page.tsx
@@ -1,4 +1,8 @@
+import { appName, headerThemeColor } from "../global_const";
 import { AppData } from "../utils/type";
+import Image from 'next/image';
+import user_icon from "@/public/user_icon.svg";
+import logo from "@/public/logo.png";
 
 function AppInsight({
     searchParams
@@ -9,7 +13,18 @@ function AppInsight({
 }){
     let appData= JSON.parse(searchParams.query) as AppData;
     return (<>
-    hi
+        <div className={`navbar sticky top-0 z-50 ${headerThemeColor}`}>
+            <div className="navbar-start">
+                <Image src={logo} alt={appName} objectFit="contain" width={250} height={80}></Image>
+            </div>
+
+            <div className="avatar placeholder navbar-end">
+                <div className="bg-transparent text-neutral-content rounded-full w-9 h-9">
+                    <Image src={user_icon} alt={'User'} className='w-10 h-10'></Image>
+                </div>
+            </div>
+
+        </div>
     </>)
 
 }
