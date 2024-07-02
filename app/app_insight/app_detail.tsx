5e5c35f65cdf602f2c55a6c290bd43d268b0655d
Erarshad <www.moarshad310@gmail.com>
2024-07-02 20:55:08 +0530

    added

--- app/app_insight/app_detail.tsx
+++ app/app_insight/app_detail.tsx
@@ -16,20 +16,20 @@ export const AppDetailTab = (data:props) => {
          
   
       <div className="container mx-auto py-8">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 
-              <ListTile icon={faCalendar} title={`Guest's visiting Date`} subtitle={new Date(data?.visitDate).toDateString()??""}></ListTile>
-              <ListTile icon={faClock} title={`Guest's visiting Time`} subtitle={new Date(data?.visitDate).toLocaleTimeString()??""}></ListTile>
-              <ListTile icon={faIdCard} title={`GuestId`} subtitle={data.guestId}></ListTile>
-              <ListTile icon={faLocation} title={`Guest's IP`} subtitle={data?.visiterDetails?.ip??""}></ListTile>
-              <ListTile icon={faMap} title={`Guest's Country`} subtitle={data?.visiterDetails?.country??""}></ListTile>
-              <ListTile icon={faGamepad} title={`Guest's Device`} subtitle={data?.visiterDetails?.device??""}></ListTile>
-              <ListTile icon={faWindowRestore} title={`Returning guest`} subtitle={(data?.visiterDetails?.isReturning==="true")?"Yes":"No"}></ListTile>
-              <ListTile icon={faIdBadge} title={`AppId`} subtitle={data.appId}></ListTile>
-                  
-                </div>
-            </div>
-  
+          <ListTile icon={faCalendar} title={`Guest's visiting Date`} subtitle={new Date(data?.visitDate).toDateString() ?? ""}></ListTile>
+          <ListTile icon={faClock} title={`Guest's visiting Time`} subtitle={new Date(data?.visitDate).toLocaleTimeString() ?? ""}></ListTile>
+          <ListTile icon={faIdCard} title={`GuestId`} subtitle={data.guestId}></ListTile>
+          <ListTile icon={faLocation} title={`Guest's IP`} subtitle={data?.visiterDetails?.ip ?? ""}></ListTile>
+          <ListTile icon={faMap} title={`Guest's Country`} subtitle={data?.visiterDetails?.country ?? ""}></ListTile>
+          <ListTile icon={faGamepad} title={`Guest's Device`} subtitle={data?.visiterDetails?.device ?? ""}></ListTile>
+          <ListTile icon={faWindowRestore} title={`Returning guest`} subtitle={(data?.visiterDetails?.isReturning === "true") ? "Yes" : "No"}></ListTile>
+          <ListTile icon={faIdBadge} title={`AppId`} subtitle={data.appId}></ListTile>
+
+        </div>
+      </div>
+
 
 
           
